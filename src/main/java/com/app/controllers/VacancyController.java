d4671a18ef5af4e2b0f097a7f87e036bd628f7bb
agaspars <82602542+agaspars@users.noreply.github.com>
2022-04-01 17:49:04 +0300

    Add vacancy service&dao

--- src/main/java/com/app/controllers/VacancyController.java
+++ src/main/java/com/app/controllers/VacancyController.java
@@ -3,10 +3,14 @@ package com.app.controllers;
 import com.app.model.jobs.Vacancy;
 import com.app.services.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class VacancyController {
@@ -27,4 +31,14 @@ public class VacancyController {
         }
     }
 
+    @GetMapping("/getVacancies/{id}")
+    public Vacancy getVacancyById(@PathVariable("id") String id)
+    {
+        try {
+            return vacancyService.getVacancyById(UUID.fromString(id));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
